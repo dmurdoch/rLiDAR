bd3898a031fd1cea5d5643ac40cc52f8e6d9f244
Caio Hamamura <caiohamamura@gmail.com>
2021-10-02 22:53:51 -0300

    * Change http protocol to https for github.io

--- R/LiDARForestStand.R
+++ R/LiDARForestStand.R
@@ -20,7 +20,7 @@
 #'@param mesh Logical, if TRUE (default) returns a tree crown mesh model, and if FALSE returns a tree crown line mode.  
 #'@return Returns a 3-D scatterplot of the individual trees as identified automatically from the LiDAR.   
 #'@author Carlos Alberto Silva and Remko Duursma. Uses code by Remko Duursma (\emph{Maeswrap} package,see "Plotstand").
-#'@references \url{http://maespa.github.io/}
+#'@references \url{https://maespa.github.io/}
 #'@examples
 #'
 #'\donttest{
